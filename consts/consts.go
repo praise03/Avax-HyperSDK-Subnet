df6c7c894efb6c39cfe5fff8a3ba506ac7f32dc6
praise03 <praise@Praise.localdomain>
2024-02-18 18:22:20 +0100

    branch

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "Rory X Chain"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "RChain"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "RCR"
 )
 
 var ID ids.ID
